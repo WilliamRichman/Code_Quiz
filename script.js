b8824bc7f99cdcd4f9785474492efafc6c99b4f6
William Richman <Goat1120@gmail.com>
2019-12-06 13:14:55 -0700

    Added the answers to the questions maybe they dont have to be arrays in arrays

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
+//Score is set to zero
+ const score = 0
+ 
+//Questions I was funny on the last one
+const questions = [
+ {
+    question: "What is JavaScript?",
+    choices:  ["A programming language\n(a)", "A tool for opening beer cans\n(b)", "A large cup of coffee?\n(c)", ],
+    answer:     "a","A programing language"
+ },
+ {
+    question: "what do you start an array with?",
+    choices: ["quotes\n(a)", "curly brackets\n(b)", "parentheses\n(c)", "square brackets\n(d)"],
+    answer:    "d","square brackets"
+ },
+ {
+    question: "What symbol do you use to indicate an ID?",
+    choices: ["#\n(a)", "||\n(b)", "&&\n(c)", "$\n(d)"],
+    answer:    "a","#"
+ },
+ {
+    question: "Can a function call itself?",
+    choices: ["Yes\n(a)", "No\n(b)"],
+    answer: "a","yes",
+ },
+ {
+   question: "Why don't I feel like I know more than I do?",
+   choices: ["You need to study more\n(a)", "You are the king of the world\n(b)", "You eat too many PBandJ sandwiches\n(c)"],
+   answer: "a","You need to study more",
+ }
+];
+
+
+
+
 const buttons = document.querySelector("#btn")
  
  
- 
+
 buttons.addEventListener("click", function () {
    /*  put what I want to happen here */
    console.log("Button works")
 })
- 
- 
- 
- 
-//Questions
-var questions = [
- {
-    question: "What is JavaScript?",
-    choices:  ["A programming language", "A tool for opening beer cans", "A large cup of coffee?", ],
-    answer:    "A programing language"
- },
- {
-    question: "what do you start an array with?",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer:    "square brackets"
- },
- {
-    question: "What symbol do you use to indicate an ID?",
-    choices: ["#", "||", "&&", "$"],
-    answer:    "#"
- },
- {
-    question: "Can a function call itself?",
-    choices: ["Yes", "No"],
-    answer: "yes",
- },
- {
-   question: "Why don't I feel like I know more than I do?",
-   choices: ["You need to study more", "You are the king of the world", "You eat too many PBandJ sandwiches"],
-   answer: "You need to study more",
- }
-];
 
