0fc638bbc0412e008addf8d9fd0373f3d0f89a12
William Richman <WRichma@mesaaz.gov>
2019-12-09 08:43:41 -0700

    added a timer i found online

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 //Score is set to zero
- const score = 0
+const score = 0
 
- //timer in sec
- const time = 150
+//timer in sec
+const time = 150
 
- const buttons = document.querySelector("#btn")
- 
+const buttons = document.querySelector("#btn")
+
 //Questions I was funny on the last one
 const questions = [
- {
-    question: "What is JavaScript?",
-    choices:  ["A programming language(a)", "A tool for opening beer cans(b)", "A large cup of coffee?(c)"], 
-    answer:     "a",
- },
- {
-    question: "what do you start an array with?",
-    choices: ["quotes(a)", "curly brackets(b)", "parentheses(c)", "square brackets(d)"],
-    answer:    "d",
- },
- {
-    question: "What symbol do you use to indicate an ID?",
-    choices: ["#(a)", "||(b)", "&&(c)", "$(d)"],
-    answer:    "a",
- },
- {
-    question: "Can a function call itself?",
-    choices: ["Yes(a)", "No(b)"],
-    answer: "a",
- },
- {
-   question: "Why don't I feel like I know more than I do?",
-   choices: ["You need to study more(a)", "You are the king of the world(b)", "You eat too many PBandJ sandwiches(c)"],
-   answer: "a"
- }
+   {
+      question: "What is JavaScript?",
+      choices: ["A programming language(a)", "A tool for opening beer cans(b)", "A large cup of coffee?(c)"],
+      answer: "a",
+   },
+   {
+      question: "what do you start an array with?",
+      choices: ["quotes(a)", "curly brackets(b)", "parentheses(c)", "square brackets(d)"],
+      answer: "d",
+   },
+   {
+      question: "What symbol do you use to indicate an ID?",
+      choices: ["#(a)", "||(b)", "&&(c)", "$(d)"],
+      answer: "a",
+   },
+   {
+      question: "Can a function call itself?",
+      choices: ["Yes(a)", "No(b)"],
+      answer: "a",
+   },
+   {
+      question: "Why don't I feel like I know more than I do?",
+      choices: ["You need to study more(a)", "You are the king of the world(b)", "You eat too many PBandJ sandwiches(c)"],
+      answer: "a"
+   }
 ];
 
 
 //The for loop for the questions or just 
-for (let i = 0; i < questions.length; i++){
+for (let i = 0; i < questions.length; i++) {
    let responce = prompt(questions[i].question);
-   if(responce === choices[i].answer){
-   //the let window.prompt(questions might need to be choice)
-   score++;
-   alert("Correct!");
-   //look to add a sound to this
-}
-   else { 
+   if (responce === choices[i].answer) {
+      //the let window.prompt(questions might need to be choice)
+      score++;
+      alert("Correct!");
+      //look to add a sound to this
+   }
+   else {
       alert("Wrong!");
       //look to add sound to this and take away 5 sec
    };
@@ -58,17 +58,74 @@ alert("you got " + score + " correct great job!")
 
 
 
- 
- 
+
+
 
 buttons.addEventListener("click", function () {
-<<<<<<< HEAD
-    start timer
-})
-=======
-   ///set inbtervule funcution
-   /*  put what I want to happen here  start the timer*/
-   console.log("Button works")
+   start timer
 })
 
->>>>>>> e16434bfb9a8ffee3c18cfaf1b6dbc4a92e8ec4a
+
+
+//I found this online
+/*  var mins = 2;
+
+        //calculate the seconds
+        var secs = mins * 60;
+
+        //countdown function is evoked when page is loaded
+        function countdown() {
+            setTimeout('Decrement()', 60);
+        }
+
+        //Decrement function decrement the value.
+        function Decrement() {
+            if (document.getElementById) {
+                minutes = document.getElementById("minutes");
+                seconds = document.getElementById("seconds");
+
+                //if less than a minute remaining
+                //Display only seconds value.
+                if (seconds < 59) {
+                    seconds.value = secs;
+                }
+
+                //Display both minutes and seconds
+                //getminutes and getseconds is used to
+                //get minutes and seconds
+                else {
+                    minutes.value = getminutes();
+                    seconds.value = getseconds();
+                }
+                //when less than a minute remaining
+                //colour of the minutes and seconds
+                //changes to red
+                if (mins < 1) {
+                    minutes.style.color = "red";
+                    seconds.style.color = "red";
+                }
+                //if seconds becomes zero,
+                //then page alert time up
+                if (mins < 0) {
+                    alert('time up');
+                    minutes.value = 0;
+                    seconds.value = 0;
+                }
+                //if seconds > 0 then seconds is decremented
+                else {
+                    secs--;
+                    setTimeout('Decrement()', 1000);
+                }
+            }
+        }
+
+        function getminutes() {
+            //minutes is seconds divided by 60, rounded down
+            mins = Math.floor(secs / 60);
+            return mins;
+        }
+
+        function getseconds() {
+            //take minutes remaining (as seconds) away
+            //from total seconds remaining
+            return secs - Math.round(mins * 60);  */
