5ff94ed1a2fe909e0a791b8c15d58d4f2b2d856c
William Richman <WRichma@mesaaz.gov>
2019-12-30 14:13:01 -0700

    added css done html done and js stared

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+
+startButton.addEventListener('click', startGame)
+nextQuestion.addEventListener('click', nextQuestion)
+
+
+function startGame() {
+    console.log('Start')
+
+}
+
+function nextQuestion() {
+    console.log('next')
+}
+
+function selectAnswer() {
+
+}
